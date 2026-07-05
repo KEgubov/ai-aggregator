0172976566895dfcb62295e60202a8f053d61ca3
KEgubov <kirilegubov@gmail.com>
2026-07-05 21:57:23 +0200

    (26) Update custom.py

--- backend/src/schemas/custom.py
+++ backend/src/schemas/custom.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class AIModelNameDTO(BaseModel):
-    name: str
+    model_name: str = Field(max_length=100, description='Model name for API')
 
+class AIModelMetaDTO(BaseModel):
+    model_id: int
+    display_name: str = Field(max_length=100, description='Display name for frontend')
+    description: str = Field(max_length=20, description='Description model for frontend')
