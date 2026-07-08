e84dbea3d8caf71af512e3e2fc2317419ab444a2
KEgubov <kirilegubov@gmail.com>
2026-07-08 13:02:16 +0200

    (33) Added exceptions.py

---
+++ backend/src/service/exceptions.py
@@ -0,0 +1,18 @@
+from sqlalchemy.exc import IntegrityError
+
+
+class DuplicateError(IntegrityError):
+    def __init__(
+        self,
+        message,
+        error_code: str = "DUPLICATE_ERROR",
+        orig: Exception | None = None,
+        params: tuple | None = None,
+        statement: str | None = None,
+    ):
+        self.message = message
+        self.error_code = error_code
+        super().__init__(statement, params, orig)
+
+    def __str__(self) -> str:
+        return f"{self.error_code}: {self.message}"
