eef56961fbc3cdbc471cc365f5177191c0faca71
KEgubov <kirilegubov@gmail.com>
2026-07-08 13:01:26 +0200

    (32) Added user_service.py

---
+++ backend/src/service/user_service.py
@@ -0,0 +1,16 @@
+from backend.src.models.orm_models import User
+from backend.src.schemas.user_schema import UserAddDTO, UserDTO
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+
+    async def user_validate(self, user: UserAddDTO) -> UserDTO | None:
+        model_user = User(**user.model_dump())
+        added_user = await self.user_repository.create_user_in_db(model_user)
+        if added_user:
+            result_dto = UserDTO.model_validate(added_user,
+                                                from_attributes=True)
+            return result_dto
+        return None
